3b3d23d343f620115b241d90f6e79697dd0cd7d1
Tim Blair <tim@bla.ir>
2010-04-03 01:40:02 +0100

    Problem 2 solved in Ruby.

---
+++ 002/ruby/solution.rb
@@ -0,0 +1,27 @@
+module Problem002
+
+  def to_fibonacci
+    return [] if self < 1
+    seq = [1,1]
+    while seq.last < self
+      seq.push(seq[-1] + seq[-2])
+    end
+    seq.pop
+    seq.shift
+    seq
+  end
+
+end
+
+class Integer
+  include Problem002
+end
+
+class Array
+  def sum
+    inject(0) { |sum,x| sum + x }
+  end
+end
+
+# calculate Fibonacci sequence then sum all the even numbers
+puts 4_000_000.to_fibonacci.reject! {|i| i % 2 != 0}.sum
