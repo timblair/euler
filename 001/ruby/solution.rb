e959acc433c64d078e41e7e8249b95e324135720
Tim Blair <tim@bla.ir>
2010-04-04 18:31:31 +0100

    Added much more efficient method of solving problem 1.

--- 001/ruby/solution.rb
+++ 001/ruby/solution.rb
@@ -10,6 +10,11 @@ module Problem001
     (multi.instance_of?(Array) ? multi : [multi]).any? { |n| self % n == 0 }
   end
 
+  def sum_divisible_by(num)
+    p = self / num
+    num*(p*(p+1))/2
+  end
+
 end
 
 class Integer
@@ -18,4 +23,12 @@ end
 
 # take total and multiples from the command line, or default if not provided
 args = (ARGV.size > 0 ? ARGV : [999, 3, 5])
-puts args.shift.to_i.sum_multiples_of(args.collect! { |n| n.to_i })
+num = args.shift.to_i
+
+# nice, clean rubyish way of doing the calculation
+puts num.sum_multiples_of(args.collect! { |n| n.to_i })
+
+# much (much) more efficient to calculate this way, but not as "clean"
+if args.length == 2
+  puts num.sum_divisible_by(args.first) + num.sum_divisible_by(args.last) - (num.sum_divisible_by(args.first*args.last))
+end
